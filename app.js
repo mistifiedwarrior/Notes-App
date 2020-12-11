8b57478c32b8b891a03d5b0bb3cc21e6cd76c52d
mistifiedwarrior <bagiballia@users.noreply.github.com>
2020-12-12 00:16:13 +0530

    Features implemented by Aditi Gupta

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-//====================VALIDATOR==================
-// const validator = require('validator');
-// console.log(validator.isEmail('angel@example.com'));
-// console.log(validator.isURL('https://www.jobsalrt.com'));
-
-//==========================CHALK=============
-// const chalk = require('chalk');
-// console.log(chalk.green.inverse('I Love My Family'));
-// console.log(chalk.white.bgRed.bold('I Love My Mother'));
-
-// console.log(process.argv[2]);
-
 const chalk = require('chalk');
 const {argv} = require('yargs');
 const yargs = require('yargs');
 const notes = require('./notes.js');
 
-// const command = process.argv[2];
-// console.log(process.argv);
-// customize yargs version=============
 yargs.version('1.1.0');
+
 // =================Create add command========
 yargs.command({
   command: 'add',
@@ -80,5 +66,4 @@ yargs.command({
     notes.readNotes(argv.title);
   },
 });
-// console.log(yargs.argv);
 yargs.parse();
